0ab3a3c71a2b19918b55bbe118f8399a715f4cc6
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2023-09-04 13:08:37 +0200

    use my AbExp branch

--- envs/abexp-absplice.post-deploy.sh
+++ envs/abexp-absplice.post-deploy.sh
@@ -4,5 +4,5 @@
 mamba install -y bioconda::cyvcf2
 
 pip install fastbetabino3 "interpret-core==0.2.7"
-PIP_NO_DEPS=1 pip install mmsplice git+https://github.com/gagneurlab/splicemap.git git+https://github.com/gagneurlab/absplice.git wget
+PIP_NO_DEPS=1 pip install mmsplice git+https://github.com/gagneurlab/splicemap.git git+https://github.com/gagneurlab/absplice.git@onnx_support wget
 
